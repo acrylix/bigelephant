0489d8a68d6dacbc0fc1d9a68aab786ce686102c
Michael <michaelyykk@gmail.com>
2016-06-10 02:03:48 -0400

    recording fix

--- www/js/controllers/test.ctrl.js
+++ www/js/controllers/test.ctrl.js
@@ -18,30 +18,41 @@ angular.module('test.controllers', ['ionic'])
 	$cordovaMedia,
 	PictureService) {
 
-	$scope.record = function() {
-		var src = "myrecording.wav";
-		var mediaRec = new Media(src, ///var/mobile/Applications/<UUID>/tmp/myrecording.wav
-			// success callback
-			function() {
-				console.log("recordAudio():Audio Success");
-			},
+	// $scope.record = function() {
+	// 	var src = "myrecording.wav";
+	// 	var mediaRec = new Media(src, ///var/mobile/Applications/<UUID>/tmp/myrecording.wav
+	// 		// success callback
+	// 		function() {
+	// 			console.log("recordAudio():Audio Success");
+	// 		},
 
-			// error callback
-			function(err) {
-				console.log("recordAudio():Audio Error: " + err.code);
-			});
+	// 		// error callback
+	// 		function(err) {
+	// 			console.log("recordAudio():Audio Error: " + err.code);
+	// 		});
 
-		// Record audio
-		mediaRec.startRecord();
+	// 	// Record audio
+	// 	mediaRec.startRecord();
 
-		// Pause Recording after 5 seconds
-		setTimeout(function() {
-			mediaRec.stopRecord();
-			console.log('paused');
-			mediaRec.play();
-		}, 5000);
+	// 	// Pause Recording after 5 seconds
+	// 	setTimeout(function() {
+	// 		mediaRec.stopRecord();
+	// 		console.log('paused');
+	// 		mediaRec.play();
+	// 	}, 5000);
 
-	}
+	// }
+
+	var mediaRec = new Media("recording.wav", ///var/mobile/Applications/<UUID>/tmp/myrecording.wav
+		// success callback
+		function() {
+			console.log("recordAudio():Audio Success");
+		},
+
+		// error callback
+		function(err) {
+			console.log("recordAudio():Audio Error: " + err.code);
+		});
 
 	//timer
 	var timer;
@@ -52,22 +63,29 @@ angular.module('test.controllers', ['ionic'])
 		// Don't start a new fight if we are already fighting
 		if (angular.isDefined(timer)) return;
 
+		mediaRec.startRecord();
 		timer = setInterval(function() {
 			$scope.recording = true;
 			$scope.time += 100;
 			$scope.timeDisp = Math.floor($scope.time / 1000 / 60) + ':' + pad(Math.floor($scope.time / 1000 % 60), 2);
 			$scope.$apply();
-			console.log($scope.time);
+			// console.log($scope.time);
 		}, 100);
 	};
 
 	$scope.stopTimer = function() {
 		if (angular.isDefined(timer)) {
+			mediaRec.stopRecord();
 			clearInterval(timer);
 			$scope.recording = false;
+			$scope.time = 0;
 			timer = undefined;
 		}
 	};
+
+	$scope.playRecording = function(){
+		mediaRec.play();
+	}
 
 	var pad = function(n, width, z) {
 		z = z || '0';
@@ -78,7 +96,7 @@ angular.module('test.controllers', ['ionic'])
 
 	$rootScope.$on('stop-timer', function(event, args) {
 		console.log('timer stopped');
-		$scope.timeDisp='';
+		$scope.timeDisp = '';
 		$scope.stopTimer();
 	});
 
