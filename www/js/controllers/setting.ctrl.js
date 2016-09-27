1c0747580685baa7e0dfcc46e2ace63799856048
acrylix <michaelyykk@gmail.com>
2016-09-26 23:30:35 -0400

    issue 4

--- www/js/controllers/setting.ctrl.js
+++ www/js/controllers/setting.ctrl.js
@@ -13,6 +13,8 @@ angular.module('setting.controllers', ['ionic'])
 		$ionicModal,
 		$cordovaFile) {
 
+		$scope.initialText = 'This is an expanding textarea.  Type something and see...';
+
 		$ionicModal.fromTemplateUrl('templates/about.html', {
 			scope: $scope,
 			animation: 'slide-in-up'
@@ -29,5 +31,49 @@ angular.module('setting.controllers', ['ionic'])
 		$scope.$on('$destroy', function() {
 			$scope.modal.remove();
 		});
+
+		$ionicModal.fromTemplateUrl('templates/feedback.html', {
+			scope: $scope,
+			animation: 'slide-in-up'
+		}).then(function(modal) {
+			$scope.feedback = modal;
+		});
+		$scope.openFeedback = function() {
+			$scope.feedback.show();
+		};
+		$scope.closeFeedback = function() {
+			$scope.feedback.hide();
+		};
+		// Cleanup the modal when we're done with it!
+		$scope.$on('$destroy', function() {
+			$scope.feedback.remove();
+		});
+
+		$scope.feedbackInfo = {
+			type: "功能",
+			content: ""
+		};
+		//send feedback
+		$scope.sendFeedback = function() {
+			if ($scope.feedbackInfo.content == "") {
+				alert("请填写反馈内容");
+			} else {
+				$rootScope.show();
+				var Feedback = AV.Object.extend('AppFeedback');
+				var feedback = new Feedback();
+				feedback.set('user', AV.User.current());
+				feedback.set('content', $scope.feedbackInfo.content);
+				feedback.set('type', $scope.feedbackInfo.type);
+				feedback.save().then(function(todo) {
+					console.log('New app feedback created with objectId: ' + todo.id);
+					$rootScope.hide();
+					alert("发送成功");
+				}, function(error) {
+					console.log('Failed to create new app feedback, with error message: ' + error.message);
+					$rootScope.hide();
+					alert("发送失败, 请检查网络");
+				});
+			}
+		}
 
 	});
