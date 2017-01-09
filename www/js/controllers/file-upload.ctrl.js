932cfbaeeeb92eb6efd947137e5fcb7f03872be3
acrylix <michaelyykk@gmail.com>
2017-01-09 01:15:08 -0500

    new uploader

--- www/js/controllers/file-upload.ctrl.js
+++ www/js/controllers/file-upload.ctrl.js
@@ -15,37 +15,58 @@ angular.module('file-upload.controllers', ['ngImageInputWithPreview'])
     function($state, $scope, $rootScope, $ionicPopup, UserService, StorageService, PictureService, $ionicPopup, $ionicHistory, $ionicNavBarDelegate, $q) { 
         $ionicNavBarDelegate.showBackButton(false);
 
+        $scope.uploadCount = 0;
+
         $scope.cancel = function() {
             $state.go('app.playlists');
         }
 
+        $scope.upload = function() {
+            var clean64 = /^data:image\/.*;base64,/;
 
+            var data = this.image.src;
+            var base64result = data.split(',')[1];
+
+            var file = new AV.File("test.jpg", { base64: base64result });
+            file.save({
+                onprogress: function(e) { console.log(e); }
+            }, {}).then(function(file) {
+                // 文件保存成功
+                alert("success");
+                console.log(file.url());
+            }, function(error) {
+                // 异常处理
+                console.error(error);
+            });
+
+
+        }
 
         $scope.previewFile = function(files) {
-            var preview = document.querySelector('img');
-            // var file = document.querySelector('input[type=file]').files[0];
-            var file = files[0];
-            var reader = new FileReader();
 
-            reader.onload = function(e) {
-            	console.log(e.target.result);
-                preview.src = e.target.result;
-            };
+            this.uploadCount = files.length;
+            this.$apply();
 
-            if (file) {
-                console.log(reader.readAsDataURL(file));
+            for (var i = 0, len = files.length; i < len; ++i) {
+                var file = files[i];
 
-                // var file = new AV.File(file.name, file);
-                // file.save({
-                //     onprogress: function(e) { console.log(e); }
-                // }, {}).then(function(file) {
-                //     // 文件保存成功
-                //     console.log(file.url());
-                // }, function(error) {
-                //     // 异常处理
-                //     console.error(error);
-                // });
+                var reader = new FileReader();
+
+                reader.onload = function(e) {
+                    // console.log(e.target.result);
+
+                    var src = e.target.result;
+
+                    var elem = angular.element(document.querySelector('#uploaderFiles'));
+                    var img_template = '<li class="weui-uploader__file" style="background-image:url(#url#)"></li>'
+
+                    elem.append(img_template.replace('#url#', src));
+
+                };
+                reader.readAsDataURL(file);
+
             }
+
         }
 
     }
