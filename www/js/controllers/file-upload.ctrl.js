08d0a4da95e97a3a1c2498c4ec2e8c36d30cd9c4
acrylix <michaelyykk@gmail.com>
2017-01-15 03:09:45 -0500

    minor fixes

--- www/js/controllers/file-upload.ctrl.js
+++ www/js/controllers/file-upload.ctrl.js
@@ -13,63 +13,55 @@ angular.module('file-upload.controllers', ['ngImageInputWithPreview'])
     '$ionicNavBarDelegate',
     '$q',
     function($state, $scope, $rootScope, $ionicPopup, UserService, StorageService, PictureService, $ionicPopup, $ionicHistory, $ionicNavBarDelegate, $q) { 
-        $ionicNavBarDelegate.showBackButton(false);
 
         $scope.uploadCount = 0;
 
-        // $scope.images = [];
         $scope.images = [];
 
+        $scope.loadingState = false;
+
+        $scope.loading = function(state) {
+            this.loadingState = state;
+            // this.$apply();
+        }
 
         $scope.cancel = function() {
             $state.go('app.playlists');
-        }
+        };
 
         $scope.save_img_cloud = function(img, id) {
             var defer = $q.defer();
 
+            // base64 process
             var blob = img.data;
             var clean64 = /^data:image\/.*;base64,/;
-
             var base64result = blob.split(',')[1];
 
             var d = new Date();
             var n = d.getTime();
-
             var file = new AV.File('pic' + n + '.jpg', { base64: base64result });
-            // console.log(n);
 
-            var img = _.findWhere(this.images, { id: id });
-            img.uploading = true;
-            console.log(img);
 
-            var onProg = function(id, e) {
-                var scope = angular.element(document.querySelector("#picture_selection_page")).scope();
+            var onProg = function(id, scope, e) {
                 var img = _.findWhere(scope.images, { id: id });
-
-
                 img.progress = Math.round(e.percent);
 
                 scope.$apply();
-                console.log(e);
             }
 
             file.save({
-                onprogress: onProg.bind(null, id)
+                onprogress: onProg.bind(null, id, this)
             }, {}).then(function(file) {
-                // 文件保存成功
-                console.warn("success");
-                // console.log();
+                console.log("img upload success");
                 defer.resolve(file.url());
             }, function(error) {
-                // 异常处理
                 console.error(error);
                 defer.reject(error);
             });
 
             return defer.promise;
 
-        }
+        };
 
         $scope.upload = function() {
             var promises = [];
@@ -83,60 +75,48 @@ angular.module('file-upload.controllers', ['ngImageInputWithPreview'])
                 $rootScope.alert('大象框', '图片上传成功!');
             }).catch(function(ex) {
                 console.error(ex);
-            })
+            });
+        };
 
+        var custom_onload = function(scope, defer, e) {
+            var src = e.target.result;
+            var id = scope.images.length + 1;
 
+            var img = {
+                id: id,
+                progress: 0,
+                data: src
+            }
 
+            scope.images.push(img);
+            defer.resolve(src);
+        };
 
-        }
-
-        var async_load = function(file) {
+        $scope.async_load = function(file) {
             var defer = $q.defer();
-
             var reader = new FileReader();
-
-            reader.onload = function(e) {
-
-                var src = e.target.result;
-
-                var scope = angular.element(document.querySelector("#picture_selection_page")).scope();
-
-                var id = scope.images.length + 1;
-
-                // var elem = angular.element(document.querySelector('#uploaderFiles'));
-                // var img_template = '<li id="pic'+id+'" class="weui-uploader__file weui-uploader__file_status" style="background-image:url(#url#)"><div class="weui-uploader__file-content">50%</div></li>'
-
-                // elem.append(img_template.replace('#url#', src));
-
-                defer.resolve(src);
-
-                var img = {
-                    id: id,
-                    progress: 0,
-                    data: src
-                }
-
-                scope.images.push(img);
-            };
+            reader.onload = custom_onload.bind(null, this, defer);
             reader.readAsDataURL(file);
             return defer.promise;
-        }
+        };
 
         $scope.previewFile = function(files) {
 
             this.uploadCount += files.length;
-            this.$apply();
+            this.loading(true);
+
+            var promises = [];
+
 
             for (var i = 0, len = files.length; i < len; ++i) {
                 var file = files[i];
-
-                async_load(file).then(function(result) {
-                    console.log("#image blob added to model");
-                })
-
+                promises.push(this.async_load(file));
             }
 
-        }
+            $q.all(promises).then(function(images) {
+                this.loading(false);
+            }.bind(this));
 
+        };
     }
 ]);
